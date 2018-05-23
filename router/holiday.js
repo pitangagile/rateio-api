441f5da2e9c8092687c63b844e1e2bed4ec9a0c8
Afonso Amorim <afonso_sa@hotmail.com>
2018-05-23 10:58:25 -0300

    [hotfix] - Holidays api, date control

--- router/holiday.js
+++ router/holiday.js
@@ -4,12 +4,19 @@ const serverless = require('serverless-http');
 const holidayController = require('../controller/holiday');
 
 const app = express();
+
+var myLogger = function (req, res, next) {
+  console.log('Midleware de validação de permissão.');
+  next();
+};
+
+app.use(myLogger);
 app.use(bodyParser.json());
 
 app.get('/api/holiday', holidayController.getIndexData);
 app.post('/api/holiday/search', holidayController.search);
 app.post('/api/holiday/create', holidayController.create);
 app.post('/api/holiday/delete', holidayController.delete_post);
-
+app.post('/api/holiday/update', holidayController.update);
 
 module.exports.routers = serverless(app);
