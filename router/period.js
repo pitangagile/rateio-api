6b53b3300486aae3b9ae263e5464a5bc5dee8a5e
Ivaldo Camelo <ivaldocamelofilho@gmail.com>
2018-05-24 10:29:12 -0300

    Creation of period routes.

--- router/period.js
+++ router/period.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
-const coastCenterController = require('../controller/coastcenter');
+const periodController = require('../controller/period');
 
 const app = express();
 app.use(bodyParser.json());
 
-
+app.get('/api/period/getAll', periodController.getAll);
+app.post('/api/period/create', periodController.create);
+app.post('/api/period/delete', periodController.delete_period);
 
 module.exports.routers = serverless(app);
