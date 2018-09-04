76123c9dd6188c488688f49dbdd6634d6e205219
Glauber Batista <glauber.batista@pitang.com>
2018-09-04 15:37:15 -0300

    Rateio V. 1

--- controller/manage/index.js
+++ controller/manage/index.js
@@ -59,51 +59,65 @@ var manageController = function (manageSchema, employeeSchema, costCenterSchema,
     }
   }
 
-  async function createManageFromEmployees(req, res) {
+  async function getAllToDownload(req, res) {
     try {
       await connectToDatabase();
 
-      let data = req.body.params.employees;
+      let period = await periodSchema.findOne({'isActive': true}).exec();
 
-      for (var i = 0; i < data.length; i++) {
-
-        let period = await periodSchema.findOne({'isActive': true}).exec();
-        let employee = await employeeSchema.findOne({'registration': data[i]['MATR']}).exec();
-        let costCenter = await costCenterSchema.findOne({'code': data[i]['COD CC ORIG']}).exec();
-
-        let newManageEmployee = new manageSchema({
-          'period.description': period.description,
-          'employee._id': employee._id,
-          'employee.name': employee.name,
-          'originCostCenter._id': costCenter._id,
-          'originCostCenter.description': costCenter.description,
-        });
-
-        await newManageEmployee.save(function (err) {
-          if (err) {
-            res.status(httpStatus.InternalServerError).send('Erro: ' + err);
+      let queryFind = {
+        $and: [
+          {
+            'period.name': period.name,
           }
-          else {
-            res.status(httpStatus.Created).end();
-          }
-        });
-      }
+        ]
+      };
 
+      var items = await manageSchema
+        .find(queryFind)
+        .sort({'employee.name': 1})
+        .exec();
+
+      res.status(httpStatus.Ok).json(items);
     } catch (e) {
       res.status(httpStatus.InternalServerError).send('Erro:' + e);
     }
   }
 
-  async function updatePeriod(period, res) {
-    period.initialManageExecuted = true;
-    await period.save(function (err) {
-      if (err) {
-        res.status(httpStatus.InternalServerError).send('Erro: ' + err);
-      }
-      else {
-        res.status(httpStatus.Created).end();
-      }
-    });
+  async function createManagesFromFile(req, res) {
+    try {
+      await connectToDatabase();
+      let data = req.body.params.employees;
+      await createManagesFromData(data);
+      res.status(httpStatus.Created).end();
+    } catch (e) {
+      res.status(httpStatus.InternalServerError).send('Erro:' + e);
+    }
+  }
+
+  async function createManagesFromData(data) {
+    for (var i = 0; i < data.length; i++) {
+
+      let period = await periodSchema.findOne({'isActive': true}).exec();
+      let employee = await employeeSchema.findOne({'registration': data[i]['MATR']}).exec();
+      let costCenter = await costCenterSchema.findOne({'code': data[i]['COD CC ORIG']}).exec();
+
+      let newManage = await new manageSchema({
+        'period.description': period.description,
+        'employee._id': employee._id,
+        'employee.name': employee.name,
+        'employee.registration': employee.registration,
+        'originCostCenter._id': costCenter._id,
+        'originCostCenter.code': costCenter.code,
+        'originCostCenter.description': costCenter.description,
+      });
+
+      await newManage.save(function (err) {
+        if (err) {
+          console.log('err > ', err);
+        }
+      });
+    }
   }
 
   async function generateManage(req, res) {
@@ -139,24 +153,17 @@ var manageController = function (manageSchema, employeeSchema, costCenterSchema,
         let qtdReportingsOfEmployee = await reportingSchema.find({'employee._id': manageFromSpreadSheet.employee._id}).count().exec();
 
         var idealHours = await employee.workHours * await calculateTotalBusinessDaysByActivePeriod();
+        let totalHoursReporting = await calculateTotalReportingHoursByUserIdAndPerActivePeriod(employee._id);
 
         // Usuário sem reportagens
         if (qtdReportingsOfEmployee === 0) {
-          if (qtdCostCentersOfEmployee === 0) {
+          if (qtdCostCentersOfEmployee === 0 || qtdCostCentersOfEmployee > 1) {
             let destinyCostCenter = originCostCenter;
-            await generateAndSaveManage(manageFromSpreadSheet, destinyCostCenter, idealHours, res);
+            await generateAndSaveManage(manageFromSpreadSheet, destinyCostCenter, idealHours, idealHours, res);
           }
-
           else if (qtdCostCentersOfEmployee === 1) {
             let destinyCostCenter = userCostCenters[0];
-            await generateAndSaveManage(manageFromSpreadSheet, destinyCostCenter, idealHours, res);
-          }
-
-          else if (qtdCostCentersOfEmployee > 1) {
-            for (var i = 0; i < userCostCenters.length; i++) {
-              let destinyCostCenter = userCostCenters[i];
-              await generateAndSaveManage(manageFromSpreadSheet, destinyCostCenter, idealHours, res);
-            }
+            await generateAndSaveManage(manageFromSpreadSheet, destinyCostCenter, idealHours, idealHours,);
           }
         }
         // Usuário com reportagens
@@ -168,47 +175,49 @@ var manageController = function (manageSchema, employeeSchema, costCenterSchema,
 
             // CC origem === CC destino
             if (originCostCenter._id === destinyCostCenter._id) {
-              await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, reportingHours, res);
+              await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, reportingHours, reportingHours);
             }
             // CC origem !== CC destino
             else {
               //  reportingHours < idealHours
               if (reportingHours < idealHours) {
-                await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, reportingHours, res);
-                await generateAndSaveManage(manageFromSpreadSheet, destinyCostCenter, await (idealHours - reportingHours), res);
+                await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, reportingHours, idealHours);
+                await generateAndSaveManage(manageFromSpreadSheet, destinyCostCenter, await (idealHours - reportingHours), idealHours);
               }
               // reportingHours >= idealHours
               else {
-                await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, reportingHours, res);
+                await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, reportingHours, reportingHours);
               }
             }
           }
           // qtdReportings > 1
           else {
-            let totalReportingHours = await calculateTotalReportingHoursByUserIdAndPerActivePeriod(employee._id);
+            // idealHours < totalHoursReporting
+            if (totalHoursReporting < idealHours) {
+              var hoursNotReporting = await (idealHours - totalHoursReporting);
+              await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, hoursNotReporting, idealHours);
 
-            console.log('idealHours > ', idealHours);
-            console.log('totalReportingHours > ', totalReportingHours);
+              for (var j = 0; j < qtdReportingsOfEmployee; j++) {
+                let newManage = await new manageSchema({
+                  'period.description': period.description,
+                  'employee._id': employee._id,
+                  'employee.registration': employee.registration,
+                  'employee.name': employee.name,
+                  'originCostCenter._id': originCostCenter._id,
+                  'originCostCenter.code': originCostCenter.code,
+                  'originCostCenter.description': originCostCenter.description,
+                });
 
-            // idealHours < totalReportingHours
-            if (totalReportingHours < idealHours) {
-              var hoursNotReporting = await (idealHours - totalReportingHours);
-              await generateAndSaveManage(manageFromSpreadSheet, originCostCenter, hoursNotReporting, res);
-            }
-
-            for (var j = 0; j < qtdReportingsOfEmployee; j++) {
-              let newManageEmployee = await new manageSchema({
-                'period.description': period.description,
-                'employee._id': employee._id,
-                'employee.name': employee.name,
-                'originCostCenter._id': originCostCenter._id,
-                'originCostCenter.description': originCostCenter.description,
-              });
-              await generateAndSaveManage(newManageEmployee, reportings[j].costCenter, reportings[j].totalHoursCostCenter, res);
+                if (totalHoursReporting < idealHours){
+                  await generateAndSaveManage(newManage, reportings[j].costCenter, reportings[j].totalHoursCostCenter, idealHours);
+                } else{
+                  await generateAndSaveManage(newManage, reportings[j].costCenter, reportings[j].totalHoursCostCenter, totalHoursReporting);
+                }
+              }
             }
           }
         }
-        await updatePeriod(period, res);
+        await updatePeriod(period);
       }
       res.status(httpStatus.Created).end();
     } catch (e) {
@@ -216,21 +225,29 @@ var manageController = function (manageSchema, employeeSchema, costCenterSchema,
     }
   }
 
-  async function generateAndSaveManage(manage, destinyCostCenter, hours, res) {
+  async function generateAndSaveManage(manage, destinyCostCenter, hours, totalHoursReporting) {
 
     manage.destinyCostCenter._id = destinyCostCenter._id;
+    manage.destinyCostCenter.code = destinyCostCenter.code;
     manage.destinyCostCenter.description = destinyCostCenter.description;
     manage.hours = hours;
+    manage.totalHoursReporting = totalHoursReporting;
+    manage.allocation = await (await (hours / totalHoursReporting) * 100);
 
-    var result = await manage.save(function (err) {
+    await manage.save(function (err) {
       if (err) {
-        res.status(httpStatus.InternalServerError).send('Erro: ' + err);
-      }
-      else {
-        res.status(httpStatus.Created).end();
+        console.log('err > ', err);
       }
     });
-    return result;
+  }
+
+  async function updatePeriod(period) {
+    period.initialManageExecuted = true;
+    await period.save(function (err) {
+      if (err) {
+        console.log('err: ' + err);
+      }
+    });
   }
 
   async function calculateTotalBusinessDaysByActivePeriod() {
@@ -319,8 +336,6 @@ var manageController = function (manageSchema, employeeSchema, costCenterSchema,
       };
       let period = await periodSchema.findOne(queryFind).exec();
 
-      console.log(period);
-
       if (period) {
         res.status(httpStatus.Ok).json(true);
       } else {
@@ -378,8 +393,9 @@ var manageController = function (manageSchema, employeeSchema, costCenterSchema,
 
   return {
     getAll: getAll,
+    getAllToDownload: getAllToDownload,
     generateManage: generateManage,
-    createManageFromEmployees: createManageFromEmployees,
+    createManagesFromFile: createManagesFromFile,
     existManageExecuted: existManageExecuted
   }
 };
--- models/manage.js
+++ models/manage.js
@@ -9,18 +9,23 @@ var manageSchema = new mongoose.Schema(
       description: {required: true, type: String}
     },
     employee: {
-      _id: { required: true, type: Schema.Types.ObjectId , ref : 'employee'},
+      _id: { required: true, type: Schema.Types.ObjectId},
+      registration: { required: true, type: String },
       name: { required: true, type: String }
     },
     originCostCenter: {
-      _id: { required: true, type: Schema.Types.ObjectId, ref : 'costCenter' },
+      _id: { required: true, type: Schema.Types.ObjectId},
+      code: {type: String, required: true},
       description : { required: true, type: String }
     },
     destinyCostCenter : {
-      _id: { type: Schema.Types.ObjectId, ref : 'costCenter' },
+      _id: { type: Schema.Types.ObjectId},
+      code: {type: String},
       description : { type: String }
     },
-    hours : {type: Number}
+    hours : {type: Number},
+    totalHoursReporting : {type: Number},
+    allocation : {type: Number}
   }, { timestamps: true }
 );
 
